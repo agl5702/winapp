aaefedca40c8cc7d2bfd0861c0fa4f1a39c4c631
agl5702 <advasquez41@misena.edu.co>
2024-03-03 21:12:34 -0500

    win commit

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
       <Auth0Provider domain={domain} clientId={clientId} authorizationParams={{redirect_uri:window.location.origin}}>
-      <App />
+      <App/>
       </Auth0Provider>
     </ChakraProvider>
   </React.StrictMode>,
